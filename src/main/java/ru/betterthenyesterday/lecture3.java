b934286ece66434e632dd522bebf520e8d376ac1
WinKeeper <winkeeper@mail.ru>
2021-01-20 22:59:04 +0500

    Trying again

---
+++ src/main/java/ru/betterthenyesterday/lecture3.java
@@ -0,0 +1,29 @@
+//package ru.betterthenyesterday;
+//
+//import java.util.Locale;
+//import java.util.Scanner; //После добавления Scanner должна появится строка, если не появилась - Выделить Scanner Alt + Enter
+//
+//public class lecture3 {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in); //Пока не паримся, создаём
+//
+//        System.out.println("Введите имя");
+//        String username = sc.nextLine();
+//        System.out.println("Вы ввели: " + username);
+//
+//        System.out.println("Введите возраст");
+//        int age = sc.nextInt();
+//        System.out.println("Возраст: " + age);
+//
+//        //doSomething(new int[]{1, 2, 3, 4, 5, 6}); //Прокидываем в метод данные
+//    }
+//
+//    public static int showUserAction(String question, String... variants) {
+//    }
+//
+//    public static void doSomething(int[] arr) {
+//        //создаём метод для массива
+//    }
+//
+//
+//}
